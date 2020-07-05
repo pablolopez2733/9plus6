07db4fc02c12ac59d95259befd90da53c2afbf69
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-07-05 06:28:26 -0500

    Perfected Plot

--- NuevaEraNBA/NewAgeNBA.R
+++ NuevaEraNBA/NewAgeNBA.R
@@ -16,16 +16,21 @@ View(GmScMp)
 
 #Gráfica top 10 gmsc
 top10 <- GmScMp[1:20,]
+png(filename="top20gmsc.png", width=1700, height=1000)
+theme_set(theme_dark(base_size = 8))
 top10_plot <- ggplot(top10, aes(x=MP, y=GmSc)) + 
-  geom_point(aes(color=Player), size=4)+
-  geom_text(label=top10$Player,vjust=1.5,hjust=.5,colour="white")+
+  geom_point(aes(color=Player), size=8)+
+  geom_text(label=top10$Player,vjust=1.5,hjust=.3,colour="white",size=8)+
   ggtitle("20 mejores actuaciones en un partido de NBA")+
   labs(
     caption = "Datos:BasketballReference.com",
     x = "Minutos Jugados",
     y = "Gamescore"
-  )
-top10_plot+theme_dark()
+  )+theme_dark()
+
+
+
+dev.off()
 
 #Ahora agregando a Harden extrapolado:
 #ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, col = Species)) + 
