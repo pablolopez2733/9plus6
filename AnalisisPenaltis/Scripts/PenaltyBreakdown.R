de0e89bece0ea790020405047ebc386b5a0e5982
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-07-24 14:46:43 -0500

    Goal divisions

--- AnalisisPenaltis/Scripts/PenaltyBreakdown.R
+++ AnalisisPenaltis/Scripts/PenaltyBreakdown.R
@@ -19,11 +19,21 @@ names(zone.df)[1] <- "Izquierda"
 names(zone.df)[2] <- "Centro"
 names(zone.df)[3] <- "Derecha"
 rownames(zone.df) <- c("Arriba","Centro","Abajo")
-#PASAMOS A MATRIZ Y HEATMAP
+#PASAMOS A MATRIZ Y HEATMAP:
+#Tiros totales---------------------------------------------------
 mypalette<-brewer.pal(9,"YlOrRd")
 matriz.zonas <- as.matrix(zone.df)
 heatmap.2( matriz.zonas, scale = "none", Rowv=FALSE, Colv=FALSE, dendrogram='none', 
            cellnote=matriz.zonas, notecol="black", trace='none', 
            key=FALSE,lwid = c(.01,.99), col=mypalette,
            lhei = c(.01,.99),margins = c(5,15 ))
-#heatmap:
+#Ahora en porcentajes:------------------------------------------
+
+zonas.porc <- round(matriz.zonas * (100/279), digits = 2)
+mypalette2<-rev(brewer.pal(9,"RdYlBu"))
+heatmap.2( zonas.porc, scale = "none", Rowv=FALSE, Colv=FALSE, dendrogram='none', 
+           cellnote=zonas.porc, notecol="black", trace='none', 
+           key=FALSE,lwid = c(.01,.99), col=mypalette2,srtCol=360, 
+           adjCol = c(0.5,1),lhei = c(.01,.99),margins = c(5,15 ))
+
+
